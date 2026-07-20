ac3ba73b834b291ba0fafb8b4a60d907e8c48782
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 11:51:47 +0530

    Add Football [A] from Codeforces

---
+++ 96/A - Football/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    int count0 = 0, count1 = 0;
+ 
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '0') {
+            count0++;
+            count1 = 0;
+        } else {
+            count1++;
+            count0 = 0;
+        }
+ 
+        if (count0 >= 7 || count1 >= 7) {
+            cout << "YES";
+            return 0;
+        }
+    }
+ 
+    cout << "NO";
+    return 0;
+}
