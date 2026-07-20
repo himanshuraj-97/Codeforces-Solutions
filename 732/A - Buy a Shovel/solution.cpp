f445b1072cd4ee16d543df5adc93c80394092819
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 11:55:38 +0530

    Add Buy a Shovel [A] from Codeforces

---
+++ 732/A - Buy a Shovel/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   int k,r;
+   cin>>k>>r;
+   int i=1;
+   while( (i*k)%10!=0&&((i*k)-r)%10!=0){
+     i++;
+   }
+   cout<<i<<endl;
+}
