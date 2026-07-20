0c71ae3f7d9da520923a64e20b28c4e36fc2df1a
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 12:06:20 +0530

    Add Fancy Fence [A] from Codeforces

---
+++ 270/A - Fancy Fence/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	   if(360%(180-n)==0&&360/(180-n)>2){
+	       cout<<"YES"<<endl;
+	   }else{
+	       cout<<"NO"<<endl;
+	   }
+	    
+	}
+ 
+}
+ 
