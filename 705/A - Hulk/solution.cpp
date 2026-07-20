0c6272c31abd86f4cda1357eccc28746ecc2db4d
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 11:52:38 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s="";
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            s+="I hate";
+        }else{
+            s+="I love";
+        }
+        if(i==n){
+            s+=" it";
+        }else
+        s+=" that ";
+    }
+    
+    cout<<s;
+    return 0;
+ 
+}
