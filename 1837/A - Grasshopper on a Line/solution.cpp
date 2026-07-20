7b4b7a97bbec3845a492a631007396d010f5012e
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 12:01:53 +0530

    Add Grasshopper on a Line [A] from Codeforces

---
+++ 1837/A - Grasshopper on a Line/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+  int t;
+  cin>>t;
+  while(t--){
+    int n,k;
+    cin>>n>>k;
+    if(n%k!=0){
+      cout<<"1"<<endl;
+      cout<<n<<endl;
+    }
+    else{
+      cout<<"2"<<endl;
+      cout<<"1 "<<n-1<<endl;
+    }
+    
+  }
+}
