a4e4b9e3ecffcc3bb5afcb99d0686eb44ed088c2
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 11:55:33 +0530

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if(n>=2){
+      cout<<"25"<<endl;
+    }if(n==1){
+      cout<<"5"<<endl;
+    }if(n==0){
+      cout<<"1"<<endl;
+    }
+}
