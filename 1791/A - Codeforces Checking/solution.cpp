80801cb0d17ac52a56b1585b30f72fa895f00642
Himanshu Kumar <himanshuraj0981@gmail.com>
2026-07-20 11:57:22 +0530

    Add Codeforces Checking [A] from Codeforces

---
+++ 1791/A - Codeforces Checking/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	  char ch;
+	  cin>>ch;
+	  if(ch=='c'||ch=='o'||ch=='d'||ch=='e'||ch=='f'||ch=='r'||ch=='e'||ch=='s'){
+	    cout<<"YES"<<endl;
+	  }else{
+	    cout<<"NO"<<endl;
+	  }
+	}
+	
+}
